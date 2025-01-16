28ab98789764c08ecdffc58b223961b00b31ac44
Peter Park <peter.park@zalando.de>
2025-01-16 23:27:46 +0100

    feat: create project

---
+++ spring-jpa-app-v1/src/main/java/com/bgpark/springjpaappv1/SpringJpaAppV1Application.java
@@ -0,0 +1,13 @@
+package com.bgpark.springjpaappv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaAppV1Application.class, args);
+	}
+
+}
---
+++ spring-jpa-app-v1/src/test/java/com/bgpark/springjpaappv1/SpringJpaAppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgpark.springjpaappv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAppV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
