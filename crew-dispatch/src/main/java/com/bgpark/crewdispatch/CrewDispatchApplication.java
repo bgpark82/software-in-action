dae55de2e4ca680d28a4de8ecf1e614f1743fc92
Peter Park <peter.park@zalando.de>
2025-01-19 17:29:32 +0100

    chore: init

---
+++ crew-dispatch/src/main/java/com/bgpark/crewdispatch/CrewDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.bgpark.crewdispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewDispatchApplication.class, args);
+	}
+
+}
---
+++ crew-dispatch/src/test/java/com/bgpark/crewdispatch/CrewDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgpark.crewdispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
